dc2d1d15fa9c05ad99c968ec02263e06218eecf5
javi <jfernandez@iter.es>
2015-06-09 11:31:13 +0100

    info3 button added

--- app/src/main/java/tenerife/rutas/jfernandez/rutasdetenerife/FragmentInfo3.java
+++ app/src/main/java/tenerife/rutas/jfernandez/rutasdetenerife/FragmentInfo3.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * Created by jfernandez on 29/05/2015.
@@ -14,6 +16,14 @@ public class FragmentInfo3 extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.info3, container, false);
+        View view = inflater.inflate(R.layout.info3, container, false);
+        Button b = (Button)view.findViewById(R.id.btContact);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity().getApplicationContext(), "Contact", Toast.LENGTH_SHORT).show();
+            }
+        });
+        return view;
     }
 }
