e2b5e24a3f7fc63fb300c6d50029ca58fb550b36
javi <jfernandez@iter.es>
2015-06-10 14:37:03 +0100

    code revised

--- app/src/main/java/tenerife/rutas/jfernandez/rutasdetenerife/FragmentDialogInfo.java
+++ app/src/main/java/tenerife/rutas/jfernandez/rutasdetenerife/FragmentDialogInfo.java
@@ -6,6 +6,8 @@ import android.support.v4.app.DialogFragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentTabHost;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
@@ -77,5 +79,56 @@ public class FragmentDialogInfo extends DialogFragment {
         MapsActivity mapsActivity = (MapsActivity) getActivity();
         mapsActivity.closeNavigationDrawer();
         super.onDismiss(dialog);
+    }
+
+    /**********/
+    protected class PagerAdapterInfo extends FragmentPagerAdapter {
+
+        public PagerAdapterInfo(FragmentManager fragmentManager){
+            super(fragmentManager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            FragmentInfo1 f;
+            switch (position){
+                case 0:
+                    FragmentInfo1 f1 = new FragmentInfo1();
+                    return f1;
+                case 1:
+                    FragmentInfo2 f2 = new FragmentInfo2();
+                    return f2;
+                //break;
+                case 2:
+                    FragmentInfo3 f3 = new FragmentInfo3();
+                    return f3;
+                default:
+                    f = new FragmentInfo1();
+
+            }
+            return f;
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            String title = "";
+            switch (position){
+                case 0:
+                    title = "Page1";
+                    break;
+                case 1:
+                    title = "Page2";
+                    break;
+                case 2:
+                    title = "Page3";
+                    break;
+            }
+            return title;
+        }
     }
 }
