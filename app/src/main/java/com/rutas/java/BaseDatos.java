eaeb9ef7eeb71993e19887ef49bce4db504caa71
jfernandez <javiredracing@gmail.com>
2015-07-23 22:50:44 +0100

    new routes and upgrade BD version

--- app/src/main/java/com/rutas/java/BaseDatos.java
+++ app/src/main/java/com/rutas/java/BaseDatos.java
@@ -18,7 +18,7 @@ public class BaseDatos extends SQLiteOpenHelper {
 	private final static String TABLA = "Senderos";
 	private final static String BASEDATOS = "BDRutas";
 	private final static String DB_PATH = "/data/data/com.rutas.java/databases/";
-	private final static int DB_VERSION = 3;	//cambiar cuando haya una nueva version de la bd
+	private final static int DB_VERSION = 4;	//cambiar cuando haya una nueva version de la bd
 	//private final static String DIRECCION = "ftp://colega:alrasaIN3@ftp.rutasdetenerife.com:20/"+ BASEDATOS;
 	
     /*String CREATE_TABLE = 
